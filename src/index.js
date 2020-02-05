ad3fd4ef5734363200ba453649acbbad92832a91
Sébastien Mischler (aka skarab) <lautr3k@users.noreply.github.com>
2020-02-05 18:05:26 +0100

    update dist

--- src/index.js
+++ src/index.js
@@ -1,15 +1,36 @@
 const parseItem = require("./parseItem");
+const i18n = require("./parser/i18n");
+
+const locales = ["fr", "us"];
+const locale = localStorage.getItem("PoELocale") || i18n.getLocale();
 
 console.log("Path of Exile Item Parser");
 
 const $body = document.querySelector("body");
 const $input = document.querySelector("#input");
 const $output = document.querySelector("#output");
+const $lang = document.querySelector("#lang");
+
+// i18n.setLocale(locale);
+
+locales.forEach(value => {
+  const $option = document.createElement("option");
+  $option.setAttribute("value", value);
+  $option.innerText = value;
+  if (locale === value) {
+    $option.setAttribute("selected", "selected");
+  }
+  $lang.appendChild($option);
+});
 
 let inputDelay = 500;
 let inputTimer = null;
 
 function getInputValue() {
+  if (!$input.value.trim().length) {
+    $output.value = "";
+    return;
+  }
   clearTimeout(inputTimer);
   inputTimer = setTimeout(function() {
     try {
@@ -22,8 +43,21 @@ function getInputValue() {
   }, inputDelay);
 }
 
-$input.value = localStorage.getItem("PoEItem"); // DEBUG
-getInputValue();
+function setLocale(locale) {
+  i18n.setLocale(locale);
+  $input.setAttribute(
+    "placeholder",
+    i18n.__("Copy/Past your item text from PoE here...")
+  );
+}
+
+$input.value = localStorage.getItem("PoEItem") || ""; // DEBUG
+
+$lang.addEventListener("change", () => {
+  localStorage.setItem("PoELocale", $lang.value); // DEBUG
+  setLocale($lang.value);
+  getInputValue();
+});
 
 $input.addEventListener("input", () => {
   localStorage.setItem("PoEItem", $input.value); // DEBUG
@@ -32,3 +66,6 @@ $input.addEventListener("input", () => {
 
 $input.addEventListener("focus", $input.select);
 $output.addEventListener("focus", $output.select);
+
+setLocale($lang.value);
+getInputValue();
