a8bb9a966965f2d5db9a787ab7e9d4469f0f29a8
Sébastien Mischler (aka skarab) <lautr3k@users.noreply.github.com>
2020-02-07 07:41:07 +0100

    Update getStats.js

--- src/parser/modules/getStats.js
+++ src/parser/modules/getStats.js
@@ -1,3 +1,28 @@
+const removeParentheses = require("../removeParentheses");
+
+const numberPattern = "[0-9]+(?:\\.[0-9]+)?";
+const typePattern = "\\([^\\)]+\\)";
+const numberRegExp = new RegExp(
+  `^(${numberPattern})(%)?(?: (${typePattern}))?$`
+);
+
+function getRange(line) {
+  let subParts = line.trim().split(" ");
+  let minMax = subParts[0].split("-");
+  let type = subParts[1];
+
+  if (type) {
+    type = removeParentheses(type);
+  }
+
+  return {
+    min: parseInt(minMax[0]),
+    max: parseInt(minMax[1]),
+    unit: "integer",
+    type
+  };
+}
+
 function getState(self, block, label) {
   const prop = block.prop(label);
 
@@ -7,6 +32,36 @@ function getState(self, block, label) {
 
   if (!self.item.stats) {
     self.item.stats = {};
+  }
+
+  const propName = prop.name.toLowerCase().replace(/ +/g, "-");
+  const matches = prop.value.match(numberRegExp);
+
+  if (matches) {
+    const value = parseFloat(matches[1]);
+    const unit = matches[2] ? "percent" : "float";
+    let type = matches[3];
+
+    if (type) {
+      type = removeParentheses(type);
+    }
+
+    self.item.stats[propName] = { value, unit, type };
+
+    return null;
+  }
+
+  const parts = prop.value.split(",");
+
+  if (parts.length === 1) {
+    self.item.stats[propName] = getRange(parts[0]);
+  } else {
+    self.item.stats[propName] = [];
+
+    parts.forEach(part => {
+      const state = getRange(part);
+      self.item.stats[propName].push(state);
+    });
   }
 }
 
@@ -21,6 +76,6 @@ module.exports = function getStats() {
   getState(this, block, "Weapon Range");
   getState(this, block, "Physical Damage");
   getState(this, block, "Elemental Damage");
-  getState(this, block, "Attacks per Second", "float");
-  getState(this, block, "Critical Strike Chance", "percent");
+  getState(this, block, "Attacks per Second");
+  getState(this, block, "Critical Strike Chance");
 };
