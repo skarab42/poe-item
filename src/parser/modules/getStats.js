b1b44b92da1e85413f785a2c5ba891f9748a0dba
Sébastien Mischler (aka skarab) <lautr3k@users.noreply.github.com>
2020-02-07 06:45:41 +0100

    get stats base

--- src/parser/modules/getStats.js
+++ src/parser/modules/getStats.js
@@ -1,3 +1,26 @@
+function getState(self, block, label) {
+  const prop = block.prop(label);
+
+  if (!prop.value) {
+    return null;
+  }
+
+  if (!self.item.stats) {
+    self.item.stats = {};
+  }
+}
+
 module.exports = function getStats() {
-  console.log("...");
+  const block = this.blocks.block(2);
+
+  if (!block) {
+    return null;
+  }
+
+  // Weapons
+  getState(this, block, "Weapon Range");
+  getState(this, block, "Physical Damage");
+  getState(this, block, "Elemental Damage");
+  getState(this, block, "Attacks per Second", "float");
+  getState(this, block, "Critical Strike Chance", "percent");
 };
