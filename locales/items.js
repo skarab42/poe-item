a868a192a036ab0576b2dfcfb2034b05b9364f14
Sébastien Mischler (aka skarab) <lautr3k@users.noreply.github.com>
2020-02-09 11:33:12 +0100

    update/create items/stats locales

--- locales/items.js
+++ locales/items.js
@@ -1,7 +1,10 @@
 import us from "./us/items.json";
+import br from "./br/items.json";
+import ru from "./ru/items.json";
+import th from "./th/items.json";
+import de from "./de/items.json";
 import fr from "./fr/items.json";
+import es from "./es/items.json";
+import kr from "./kr/items.json";
 
-export default {
-  us,
-  fr
-};
+export default { us, br, ru, th, de, fr, es, kr };
---
+++ locales/stats.js
@@ -0,0 +1,10 @@
+import us from "./us/stats.json";
+import br from "./br/stats.json";
+import ru from "./ru/stats.json";
+import th from "./th/stats.json";
+import de from "./de/stats.json";
+import fr from "./fr/stats.json";
+import es from "./es/stats.json";
+import kr from "./kr/stats.json";
+
+export default { us, br, ru, th, de, fr, es, kr };
