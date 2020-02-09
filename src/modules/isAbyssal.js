7bdf513f78e8c6c645f368f2dde4b96dc9f3ca30
Sébastien Mischler (aka skarab) <lautr3k@users.noreply.github.com>
2020-02-09 16:49:08 +0100

    Delete isAbyssal.js

--- src/modules/isAbyssal.js
+++
@@ -1,8 +0,0 @@
-export default function isAbyssal() {
-  const abyssal = this.__("Abyssal");
-  const matches = this.blocks.match(`^${abyssal} *$|^${abyssal} (.+)$`);
-
-  if (matches) {
-    this.item.isAbyssal = true;
-  }
-}
