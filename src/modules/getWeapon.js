8df54e7ac46b55ea1d570e89a4010ea7998b02cb
Sébastien Mischler (aka skarab) <lautr3k@users.noreply.github.com>
2020-02-09 09:51:38 +0100

    Update getWeapon.js

--- src/modules/getWeapon.js
+++ src/modules/getWeapon.js
@@ -11,14 +11,8 @@ export default function getWeapon() {
     const handsPattern = `${oneHanded}|${twoHanded}`;
     const matches = line.match(`^(${handsPattern})?(?: ?)(.+)$`, "i");
     const hands = matches[1] === twoHanded ? 2 : 1;
-    const category = matches[2];
+    const type = matches[2];
 
-    this.item.weapon = { category };
-
-    if (this.item.type !== category) {
-      this.item.weapon.subCategory = this.item.type;
-    }
-
-    this.item.weapon.hands = hands;
+    this.item.weapon = { type, hands };
   }
 }
